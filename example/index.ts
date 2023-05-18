3b35aee570f6000b1abf5326f2a4b3ebed70d2b8
tsviatkov <nick.tsviatkov@gmail.com>
2023-05-18 21:08:28 +0300

    Rename the variable

--- example/index.ts
+++ example/index.ts
@@ -1,17 +1,17 @@
 import {OpenfortClient} from "@openfort/openfort-node";
 
 async function example() {
-    const openfortClient = new OpenfortClient("sk_test_XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX");
-    const players = await openfortClient.players.getPlayers();
+    const openfort = new OpenfortClient("sk_test_XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX");
+    const players = await openfort.players.getPlayers();
     for (const player of players.body.data) {
         console.log(player.id);
     }
 
-    const newPlayer = await openfortClient.players.createPlayer("Test");
+    const newPlayer = await openfort.players.createPlayer("Test");
     console.log(newPlayer.body.id)
-    await openfortClient.players.createSession(newPlayer.body.id, "public key");
+    await openfort.players.createSession(newPlayer.body.id, "public key");
 
-    const transactionIntents = await openfortClient.transactions.getTransactionIntents();
+    const transactionIntents = await openfort.transactions.getTransactionIntents();
     for (const intent of transactionIntents.body.data) {
         console.log(`Intent ${intent.id} by ${intent.player}`);
     }
