8cd114263d839d28597c799bcfba7b25bf3500f7
Nick Tsviatkov <nick.tsviatkov@gmail.com>
2023-09-05 15:47:53 +0200

    Fixed formatting

--- example/webhook/index.ts
+++ example/webhook/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import Openfort from "@openfort/openfort-node";
 
@@ -6,21 +6,24 @@ dotenv.config();
 const app = express();
 const port = getEnvVariable("APP_PORT");
 
-app.post('/webhook/openfort', express.raw({ type: "application/json" }), async (req: Request, res: Response) => {
-  console.log(req.headers)
-  const openfort = new Openfort(getEnvVariable("OPENFORT_APIKEY"), process.env.OPENFORT_BASEURL);
-  const event = await openfort.constructWebhookEvent(req.body.toString(), req.headers["openfort-signature"]);
-  console.log(event.data.id);
+app.post("/webhook/openfort", express.raw({ type: "application/json" }), async (req: Request, res: Response) => {
+    const openfort = new Openfort(getEnvVariable("OPENFORT_APIKEY"), process.env.OPENFORT_BASEURL);
+    try {
+        const event = await openfort.constructWebhookEvent(req.body.toString(), req.headers["openfort-signature"]);
+        console.info(event.data.id);
+    } catch (e) {
+        console.error((e as Error).message);
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 function getEnvVariable(name: string): string {
-  const result = process.env[name];
-  if (!result) {
-      throw Error(`Environment variable is missing: ${name}`);
-  }
-  return result;
+    const result = process.env[name];
+    if (!result) {
+        throw Error(`Environment variable is missing: ${name}`);
+    }
+    return result;
 }
