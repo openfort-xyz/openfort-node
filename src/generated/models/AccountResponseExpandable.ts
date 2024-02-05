8777c38426144f1f3b61586b23061b8b27fc8a84
Jaume Alavedra <jamalavedra@gmail.com>
2024-02-05 16:40:56 +0100

    add player in response of account

--- src/generated/models/AccountResponseExpandable.ts
+++ src/generated/models/AccountResponseExpandable.ts
@@ -12,4 +12,4 @@
 
 import { HttpFile } from '../http/http';
 
-export type AccountResponseExpandable = "transactionIntents" ;
+export type AccountResponseExpandable = "transactionIntents" | "player" ;
