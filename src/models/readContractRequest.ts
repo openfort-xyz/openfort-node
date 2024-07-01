5dfab66e400159b0b2503800743a55fac16f0cd5
Jaume Alavedra <jamalavedra@gmail.com>
2024-07-01 12:59:00 +0200

    fix: read contract

--- src/models/readContractRequest.ts
+++ src/models/readContractRequest.ts
@@ -3,5 +3,5 @@ import { ContractReadQueries as ApiContractReadQueries } from "../generated";
 export interface ReadContractRequest extends ApiContractReadQueries {
     id: string;
     functionName: string;
-    functionArgs?: any[];
+    functionArgs?: string[];
 }
